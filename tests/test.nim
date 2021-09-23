b3fca1da8ccaa816429758f15eeeb992d5a4b0a8
shayanhabibi <shayanftw@gmail.com>
2021-09-23 16:57:59 +0800

    wth

--- tests/test.nim
+++ tests/test.nim
@@ -1,3 +1,6 @@
+## Want to see if variants only present the fields that are actually
+## present or if they just keep hold of all of them
+
 import amphetanim/spec
 
 type
@@ -17,30 +20,51 @@ type
 var p = P(m: false, x: 5)
 var i = I(m: false, x: 8, k: true, a: 3)
 
-let t = getAmpheType i
-echo t.flags
-echo t.kind
+template getMeType(o: typed): ptr TNimType =
+  cast[PNimType](getTypeInfo(o))
 
-if not t.base.isNil():
-  let b = t.base[]
-  let n = b.node[]
-  echo n.name
-  if not n.sons.isNil:
-    let sons = n.sons[]
-    for p in sons:
-      if p.isNil:
+
+
+var t = getMeType i
+proc woahImpl(x: ptr TNimType)
+
+proc echoChildNodesImpl(x: TNimNode)
+
+proc echoChildNodes(x: TNimNode) =
+  # Lets print the name of the node
+  echo x.name
+  # Check if its got sons
+  if not x.sons.isNil:
+    for son in x.sons[]:
+      if son.isNil:
         break
-      let n = p[]
-      # echo n.name
-  echo n.len
+      # Any sons that it has we want to do the same
+      echoChildNodesImpl(son[])
+      if not son.typ.isNil:
+        # If it has a type then we'll chuck that in to get checked for nodes
+        woahImpl(son.typ)
 
-let n = t.node[]
-echo n.name
-echo n.len
-let sons = n.sons[]
+proc echoChildNodesImpl(x: TNimNode) =
+  echoChildNodes x
 
-for p in sons:
-  if p.isNil:
-    break
-  let n = p[]
-  echo n.name
+proc woah(t: ptr TNimType) =
+  block:
+    # We'll check the ptr to the type to see if its nil
+    if t.isNil:
+      break
+    # If not we'll check to see if it has a node; break else
+    if t[].node.isNil:
+      break
+    # Now lets iterate over that node 
+    echoChildNodes t[].node[]
+    # Also check if theres a base type
+    if not t[].base.isNil:
+      # If there is lets check its nodes ;_;
+      woahImpl(t[].base)
+
+
+
+proc woahImpl(x: ptr TNimType) =
+  woah(x)
+
+woah t
