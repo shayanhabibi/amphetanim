80d680fd62c1ab78e5aa93a830677e541bd8b744
Shayan Habibi <shayanftw@gmail.com>
2021-09-25 00:58:34 +0800

    Tired of progress

--- amphetanim.nim
+++ amphetanim.nim
@@ -8,5 +8,53 @@
 #       an ordered list of pointers which we can then invalidate every cache line
 #       for
 
-import assume/typeit
+import std/macros
 
+import assume/spec
+
+type
+  ItOption* = enum
+    itRefsOnly = "ignore any object that isnt ref boi"
+
+type
+  MyObj = ref object of RootObj
+    x: string
+  Obj = ref object of MyObj
+    y: string
+  Cell = object
+    addrs: int
+    size: int
+    align: int
+  CellHeap = seq[Cell]
+  
+var obj: Obj = Obj(x: "hi", y: "bye")
+
+echo sizeof(obj.y)
+echo repr cast[int](obj)
+
+for key,field in obj[].fieldPairs:
+  echo ""
+  echo key
+  echo cast[int](field.unsafeAddr)
+
+echo sizeof(obj[])
+
+import amphetanim/spec as aspec
+
+echo cast[int](getTypeInfo(obj))
+echo cast[int](getTypeInfo(obj[]))
+var t = getAmpheType obj[]
+
+echo t
+
+template getAddrSizeAlign(node: ref): (int, int, int) = # Can use lower byte ints?
+  var addrs = cast[int](node)
+  var t = getAmpheType node
+  
+  (addrs, t.size, t.align)
+
+template getAddrSizeAlign(node: var object): (int, int, int) =
+  var addrs = cast[int](node.addr)
+  var t = getAmpheType node
+
+  (addrs, t.size, t.align)
